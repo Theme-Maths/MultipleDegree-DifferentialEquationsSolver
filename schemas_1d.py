7deba83400abbcea761519942ee2b27b5806d3cc
ju3210 <80517089+ju3210@users.noreply.github.com>
2021-03-17 00:32:20 +0100

    Update schemas_1d.py

--- schemas_1d.py
+++ schemas_1d.py
@@ -13,11 +13,10 @@ Contient les fonctions de résolution en 1D :
 
 
 
-def euler(f, y0, t0, T, h, digits=10):
+def euler(f, y0, t0, T, h):
     """
     Résolution d'une EDO 1D par la méthoe d'Euler explicite.
     (RAPPEL Schéma d'Euler : y_k+1 = y_k + f(y_k, t_k) * h)
-
     Paramètres
     ----------
     f  :    fonction du problème de Cauchy associé (valeur de f')
@@ -26,12 +25,10 @@ def euler(f, y0, t0, T, h, digits=10):
     T  :    valeur de la date T (borne sup de l'intevalle de résolution')
     h  :    valeur du pas h
     digits :(optionel) nombre de décimales des arrondis, 10 par défaut
-
     Renvoi
     -------
     t_list : liste des valeurs de t
     y_list : liste des valeurs de y correspondantes calculées par le schéma
-
     """
 
     t_list = [t0]                        #initialisation d'une liste avec les valeurs de t
@@ -40,11 +37,11 @@ def euler(f, y0, t0, T, h, digits=10):
     y = y0                               #initialisation de y à y0, la condition initiale
 
     while t < T:                         #Tant que la date t est inférieure à la date T
-       t = t + h                         #on incrémente la date avec le pas de calcul h
-       t_list.append(round(t,digits))    #on ajoute cette nouvelle date à la fin de la liste des dates (round = arrondi à "digits" décimales)
-
-       y = y + f(t, y)*h                 #on calcula nouvelle valeur de y
-       y_list.append(round(y,digits))    #on ajoute le nouveau y à la fin de la liste des y
+        t = t + h                         #on incrémente la date avec le pas de calcul h
+        t_list.append(t)                  #on ajoute cette nouvelle date à la fin de la liste des dates
+ 
+        y = y + f(t, y)*h                 #on calcula nouvelle valeur de y
+        y_list.append(y)                  #on ajoute le nouveau y à la fin de la liste des y
 
     return t_list, y_list
 
@@ -53,7 +50,6 @@ def euler_vect(F, Y0, t0, T, h):
     """
     Résolution d'une EDO 1D par la méthoe d'Euler explicite sous forme vectorielle.
     (RAPPEL Schéma d'Euler : y_k+1 = y_k + f(y_k, t_k) * h)
-
     Paramètres
     ----------
     F  :    fonction du problème de Cauchy associé
@@ -61,12 +57,10 @@ def euler_vect(F, Y0, t0, T, h):
     t0 :    valeur de la date t0 (borne inf de l'intervalle de résolution)
     T  :    valeur de la date T (borne sup de l'intevalle de résolution')
     h  :    valeur du pas h
-
     Renvoi
     -------
     t_list : liste des valeurs de t
     Y_list : liste des valeurs de y correspondantes calculées par le schéma
-
     """
 
     t_list = [t0]                                       #initialisation d'une liste avec les valeurs de t
@@ -84,7 +78,7 @@ def euler_vect(F, Y0, t0, T, h):
     return t_list, [Y_list[i][1] for i in range(len(Y_list))]
 
 
-def runge_kutta_4(f, y0, t0, T, h, digits=10) :
+def runge_kutta_4(f, y0, t0, T, h) :
     """
     Résolution d'une EDO 1D par la méthoe de RK4.
     (RAPPEL Schéma d'Euler : y_i1 = y_i + h/6*(k1+2*k2+2*k3+k4)
@@ -103,12 +97,10 @@ def runge_kutta_4(f, y0, t0, T, h, digits=10) :
     h  :    valeur du pas h
     k1, k2, k3, k4 : coefficients de la méthode de RK4
     digits :(optionel) nombre de décimales des arrondis, 10 par défaut
-
     Renvoi
     -------
     t_list : liste des valeurs de t
     y_list : liste des valeurs de y correspondantes calculées par le schéma
-
     """
     t_list = [t0]                        #initialisation d'une liste avec les valeurs de t
     y_list = [y0]                        #initialisation d'une liste avec les valeurs de y
@@ -116,41 +108,39 @@ def runge_kutta_4(f, y0, t0, T, h, digits=10) :
     y = y0                               #initialisation de y à y0, la condition initiale
 
     while t < T:                         #Tant que la date t est inférieure à la date T
-       t = t + h                         #on incrémente la date avec le pas de calcul h
-       t_list.append(round(t,digits))    #on ajoute cette nouvelle date à la fin de la liste des dates (round = arrondi à "digits" décimales)
+        t = t + h                         #on incrémente la date avec le pas de calcul h
+        t_list.append(t)                  #on ajoute cette nouvelle date à la fin de la liste des dates (round = arrondi à "digits" décimales)
 
-       k1=f(t,y)                         #on calcule à chaque fois les coefficients k1, k2, k3, k4 pour la méthode RK4
-       k2=f(t+h/2,y+k1*h/2)
-       k3=f(t+h/2,y+k2*h/2)
-       k4=f(t+h,y+h*k3)
+        k1 = f(t,y)                         #on calcule à chaque fois les coefficients k1, k2, k3, k4 pour la méthode RK4
+        k2 = f(t+h/2, y+k1*h/2)
+        k3 = f(t+h/2, y+k2*h/2)
+        k4 = f(t+h, y+h*k3)
        
-       y = y + h/6*(k1+2*k2+2*k3+k4)     #on calcule la nouvelle valeur de y
-       y_list.append(round(y,digits))    #on ajoute le nouveau y à la fin de la liste des y
+        y = y + h/6*(k1+2*k2+2*k3+k4)     #on calcule la nouvelle valeur de y
+        y_list.append(y)                  #on ajoute le nouveau y à la fin de la liste des y
 
     return t_list, y_list
 
-def rk4_vect(F, Y0, t0, T, h, digits=10) :
+
+def rk4_vect(F, Y0, t0, T, h) :
+    
     t_list = [t0]                        #initialisation d'une liste avec les valeurs de t
     Y_list = [Y0]                        #initialisation d'une liste avec les valeurs de y
     t = t0                               #initialisation de t à t0, la borne inf de l'intervalle de résolution
     Y = Y0                               #initialisation de y à y0, la condition initiale
 
     while t < T:                         #Tant que la date t est inférieure à la date T
-       t = t + h                         #on incrémente la date avec le pas de calcul h
-       t_list.append(t)    #on ajoute cette nouvelle date à la fin de la liste des dates (round = arrondi à "digits" décimales)
+        t = t + h                        #on incrémente la date avec le pas de calcul h
+        t_list.append(t)                 #on ajoute cette nouvelle date à la fin de la liste des dates
 
-       k1=F(t,Y)                         #on calcule à chaque fois les coefficients k1, k2, k3, k4 pour la méthode RK4
-       k2=F(t+(h/2),Y+k1*(h/2))
-       k3=F(t+h/2,Y+k2*(h/2))
-       k4=F(t+h,Y+h*k3)
-       k = [k1, k2, k3, k4]
-       Y = [ y + (h/6)*(k1+2*k2+2*k3+k4) for y, yp in zip(Y, F(t, Y)) ]
-       Y = Y + (h/6)*(k1+2*k2+2*k3+k4)     #on calcule la nouvelle valeur de y
-       Y_list.append(Y)    #on ajoute le nouveau y à la fin de la liste des y
+        k1 = F(t, Y)
+        k2 = F(t + h/2, [Y[i] + k1[i]*h/2 for i in range(len(Y))])
+        k3 = F(t + h/2, [Y[i] + k2[i]*h/2 for i in range(len(Y))])
+        k4 = F(t + h, [Y[i] + h*k3[i] for i in range(len(Y))])
+        
+        Y = [Y[i] + (h/6)*(k1[i] + 2*k2[i] + 2*k3[i] + k4[i]) for i in range(len(Y))]   #on calcule la nouvelle valeur de Y
+            
+        Y_list.append(Y)                 #on ajoute le nouveau y à la fin de la liste des y
 
-    return t_list, Y_list
-    
-
-
-    
-    
+        
+    return t_list, [Y_list[i][1] for i in range(len(Y_list))]
