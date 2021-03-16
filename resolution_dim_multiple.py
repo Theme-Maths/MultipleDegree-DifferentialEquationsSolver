d1a7509094ded8129805b80295645a1004ea4a5d
ju3210 <80517089+ju3210@users.noreply.github.com>
2021-03-17 00:31:39 +0100

    Update resolution_dim_multiple.py

--- resolution_dim_multiple.py
+++ resolution_dim_multiple.py
@@ -13,7 +13,7 @@ import math
 
 
 
-def solution_dim_2(g, y0, yp0, t0, T, h):
+def solution_dim_2(g, y0, yp0, t0, T, h, methode='rk4'):
     """
     Résout une équation différentielle d'odre 2 avec la méthode choisie.
     Renvoie un tuple x, y de la solution calculée.
@@ -26,6 +26,7 @@ def solution_dim_2(g, y0, yp0, t0, T, h):
     t0  : valeur de la borne inférieure de l'intervalle de résolution
     T   : valeur de la borne supérieure de l'intervalle de résolution
     h   : valeur du pas
+    methode : méthodes de résolution possibles : 'euler', 'rk4'. Vaut 'rk4' par défaut.
 
     Returns
     -------
@@ -44,7 +45,14 @@ def solution_dim_2(g, y0, yp0, t0, T, h):
         return [yp, ypp]                                            # on renvoie le vecteur (y', y")
     
     Y0  = [y0, yp0]                                                 # on construit le vecteur des conditions initiales
-    t_list, Y_list = schemas_1d.euler_vect(F, Y0, t0, T, h)         # on appelle une fonction 1D pour résoudre l'équation 1D vectorielle obtenue
+    
+    if methode == 'rk4':                                             # on appelle une fonction 1D pour résoudre l'équation 1D vectorielle obtenue
+        t_list, Y_list = schemas_1d.rk4_vect(F, Y0, t0, T, h)
+    elif methode == 'euler':
+        t_list, Y_list = schemas_1d.euler_vect(F, Y0, t0, T, h)
+    else :
+        raise ValueError("Méthode non reconnue. Méthodes connues : 'rk4', 'euler'.")
+        
     return t_list, Y_list
 
 
@@ -69,14 +77,14 @@ def sol_exacte_dim_2(g, y0, yp0, t0, T, h):
     Y_list : liste des ordonnées de la 'solution exacte' trouvée
 
     """
-    def F(Y, t):                                                    # on définit la fonction F du nouveau problème de Cauchy
+    def F(t, Y):                                                    # on définit la fonction F du nouveau problème de Cauchy
         (y, yp) = Y                                                 # on explicite les composantes de Y = (y, y')
         ypp = g(t, y, yp)                                           # on calcule la dernière coordonnée de ce que renverra F, à savoir ypp
         return np.array([yp, ypp])                                  # on renvoie le vecteur (y', y")
     
     Y0 = np.array([y0, yp0])                                        # on construit le vecteur des conditions initiales
     x = np.linspace(t0, T, int((T-t0)/h))                           # on construit la liste des abscisses
-    Y = odeint(F, Y0, x)                                            # on appelle scipy.integrate.odeint pour résoudre le système (utilisant une version améliorée de RK4)
+    Y = odeint(F, Y0, x, tfirst=True)                               # on appelle scipy.integrate.odeint pour résoudre le système (utilisant une version améliorée de RK4)
     return x, Y[:,1]
 
 #%%                             COMMANDES DIRECTES
@@ -85,11 +93,11 @@ def sol_exacte_dim_2(g, y0, yp0, t0, T, h):
 g = lambda t, y, yp : -y
 
 # Définition des courbes que l'on veut tracer
-x1, y1 = solution_dim_2(g, 1, 0, 0, 30, 0.1)
-x2, y2 = solution_dim_2(g, 1, 0, 0, 10, 0.5)
-x3, y3 = solution_dim_2(g, 1, 0, 0, 5, 1)
-xs, ys = sol_exacte_dim_2(g, 1, 0, 0, 30, 0.01)
+x1, y1 = solution_dim_2(g, 1, 1, 0, 10, 0.1, methode='euler')
+x2, y2 = solution_dim_2(g, 1, 1, 0, 10, 0.1, methode='rk4')
+x3, y3 = solution_dim_2(g, 1, 1, 0, 10, 1, methode='rk4')
+xs, ys = sol_exacte_dim_2(g, 1, 1, 0, 10, 0.01)
 
 # Traçage des courbes
 import traces
-traces.trace((x1, y1, 'pas de 0.1'), (x2, y2, 'pas de 0.5'), (x3, y3, 'pas de 1'), sol=(xs, ys))
+traces.trace((x1, y1, 'Euler'), (x2, y2, 'RK4'), (x3, y3, 'RK4 pas de 1'), sol=(xs, ys))
