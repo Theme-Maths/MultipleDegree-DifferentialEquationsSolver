f072dc770708d2226da33327df34a62b3fa4dfab
ju3210 <80517089+ju3210@users.noreply.github.com>
2021-04-17 19:26:27 +0200

    Update traces.py

--- traces.py
+++ traces.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 """
+Ce fichier gère l'affichage des courbes sur un même graphique (de 1 à 6).
+
 Contient les fonctions de tracés :
-    - trace(x, *Y, sol=None):
-        Trace un graphe représentant une à six listes et éventuellement la solution réelle.
+    - trace(x, *Y, sol=None)
 """
 
 import matplotlib.pyplot as plt
@@ -10,28 +11,28 @@ import matplotlib.pyplot as plt
 
 def trace(*courbe, sol=None):
     """
-    Trace un graphe représentant une à six listes et éventuellement la solution réelle.
+    Trace un graphe représentant une à six listes et éventuellement la solution 'exacte'.
 
     Paramètres
     ----------
     *courbe : jusqu'à 6 tuples (x, y, name) où x et y sont des listes de valeurs et 'name' est une chaîne qui légendera la courbe x,y.
-    sol     : (optionnel) couple (x,y) de la solution réelle, qui sera tracée aussi. Vaut None par défaut.
+    sol     : (optionnel) tuple (x,y) de la solution 'exacte' de référence, qui sera tracée aussi. Vaut None par défaut.
     """
     
-    colors = "bgrycm"                                               #initiales des couleurs reconnues par Python
-    if len(courbe) > 6:                                             #si l'utilisateur a demandé de tracer plus de 6 courbes, on lève une erreur
+    colors = "bgrycm"                     # liste des couleurs reconnues par matplotlib
+    if len(courbe) > 6:                   # Si l'utilisateur a demandé de tracer plus de 6 courbes, on lève une erreur
         raise IndexError("Impossible de tracer plus de 6 courbes.")
     
-    for (x, y, name) in courbe:                                     #Pour toutes les courbes demandées
-        plt.plot(x, y, "{}-+".format(colors[courbe.index((x, y, name))]), label=name)
-        #on entre la courbe correspondante en paramétrant l'affichage : une couleur parmi celles reconnues par Python ; - pour ligne continue ; + pour les marqueurs en forme de plus
+    for (x, y, name) in courbe:
+        plt.plot(x, y, "{}-".format(colors[courbe.index((x, y, name))]), label=name)
+        # on trace les courbes en paramétrant l'affichage avec une chaîne : une couleur parmi celles reconnues par Python ; - pour ligne continue
     
-    if sol != None:                                                 #Si l'utilisateur a entré une solution exacte 
-        plt.plot(sol[0], sol[1], "k--", label="solution exacte")
-        #on l'entre avec les paramètres suivants : k pour black ; - - pour ligne pointillée
+    if sol != None:                        # Si l'utilisateur a entré une solution exacte 
+        plt.plot(sol[0], sol[1], "k--", label="Solution exacte")
+        # on la trace avec les paramètres suivants : k pour black ; - - pour ligne pointillée
 
-    plt.xlabel("temps")                                             #nom de l'axe des abscisses
-    plt.ylabel("valeur")                                                 #nom de l'axe des ordonnées
-    plt.legend()                                                    #localisation de la légende dans le coin en bas à droite
- 
-    plt.show()                                                      #on montre le graphique
+    plt.xlabel("temps")
+    plt.ylabel("valeur")
+    plt.legend()
+    
+    plt.show()
