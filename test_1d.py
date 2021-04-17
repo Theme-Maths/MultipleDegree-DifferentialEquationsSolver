ac5117a076dc4d079a0e46e0db3734259f7eeff7
ju3210 <80517089+ju3210@users.noreply.github.com>
2021-04-17 19:27:14 +0200

    Update test_1d.py

--- test_1d.py
+++ test_1d.py
@@ -1,40 +1,33 @@
 # -*- coding: utf-8 -*-
 """
-Utiliser ce code pour calculer une solution 1d et la tracer. L'exemple existant est celui de la chute d'un corps.
+Utiliser ce code pour calculer une solution 1d et la tracer.
+Conçu en cellules pour faciliter la déclaration des paramètre.
+L'exemple ici présenté est celui de la chute des corps.
 
 - La cellule PANNEAU DE COMMANDE DES PARAMETRES permet de déclarer les variables du problème.
-- La cellule APPELS AUX FONCTIONS se charge toute seule d'appeler les fonctions souhaitées.
-  Elle ne doit être modifiée que si on veut tracer plusieurs courbes, car cela implique de bidouiller les informations données à des fonctions stockées dans d'autres fichiers :)
-"""
+- La cellule APPELS AUX FONCTIONS se charge d'appeler les fonctions souhaitées.
+  """
+  
+# Cellule à éxécuter au moins une fois avec Ctrl+Enter pour importer les modules.
+
 import schemas_1d, traces
 import numpy as np
 from scipy.integrate import odeint
 
-
 #%%                     PANNEAU DE COMMANDE DES PARAMETRES
 
-t0, T = 0, 30                                   #initialisation de l'intervalle de résolution
-h = 1                                           #pas
-y0 = 0                                          #solution initiale  
-digits = 5                                      #précison des arrondis
-f = lambda t, y : 0 - 0.00203 * y**2 + 9.81     #fonction de Cauchy (expression de f')
+# Cellule à éxécuter au moins une fois avec Ctrl+Enter pour que les paramètres soient pris en compte.
 
-# Cellule à éxécuter une fois avec Ctrl+Enter avant d'appeler les fonctions ci-dessus pour que les paramètres soient pris en compte.
-
-
+t0, T = 0, 30                                   # intervalle de résolution
+h = 1                                           # pas
+y0 = 0                                          # solution initiale  
+f = lambda t, y : 0 - 0.00203 * y**2 + 9.81     # fonction du problème de Cauchy (expression de f')
 
 
 #%%                          APPELS AUX FONCTIONS
 
-# Cellule à modifier seulement si on veut tracer plus d'une fonction sur un graphe.
-
-(x, y) = schemas_1d.euler(f, y0, t0, T, h, digits=10)
-traces.trace((x, y, "Méthode d'Euler"))
-
-"""Tracé de RK4"""
-(x, y) = schemas_1d.runge_kutta_4(f, y0, t0, T, h, digits=10)  #Méthode RK4
-(a, b) = schemas_1d.euler(f, y0, t0, T, h, digits=10)
-c = np.linspace(t0, T, 1000)
-d = odeint(f, y0, c)
-traces.trace((x, y, "Méthode RK4"),(a, b, "Méthode d'Euler"),sol =(c,d))
-
+x_rk, y_rk = schemas_1d.runge_kutta_4(f, y0, t0, T, h)
+x_eu, y_eu = schemas_1d.euler(f, y0, t0, T, h)
+x = np.linspace(t0, T, 1000)
+y = odeint(f, y0, x, tfirst = True)
+traces.trace((x_eu, y_eu, "Méthode Euler"), (x_rk, y_rk, "Méthode RK4"), sol = (x, y))
